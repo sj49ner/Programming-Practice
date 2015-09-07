4d6dca729c57f6f781ea50a39629fd5d6973c5b2
sj49ner <sj49ner@gmail.com>
2015-09-07 00:58:34 -0700

    Create SampathLoopsFactorial.java

---
+++ SampathLoopsFactorial.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class SampathLoopsFactorial {
+	public static void main(String args[]){
+		int factorial=1;
+		for(int i=1; i<=3;i++){
+			factorial=factorial*i;
+		}
+	System.out.println("The factorial result answer is " + factorial);
+		
+	}
+}
