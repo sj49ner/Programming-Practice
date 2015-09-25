614073f02ab3ab1e225c448b66719fc3196acda2
sj49ner <sj49ner@gmail.com>
2015-09-24 17:33:05 -0700

    Create SampathStringReverseWordsInSentence.java

---
+++ SampathStringReverseWordsInSentence.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SampathStringReverseWordsInSentence {
+
+	public static void main(String[] args) {
+		String str[]="He is the one".split(" ");
+		String finalStr="";
+			for(int i=str.length-1; i>=0; i--){
+				finalStr+= str[i]+" ";
+			}
+			System.out.println(finalStr);
+	}
+
+}
