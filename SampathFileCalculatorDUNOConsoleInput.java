183c6d16d63d3ccee7f4d107c429ddb2c17dc363
sj49ner <sj49ner@gmail.com>
2015-09-24 17:36:36 -0700

    Create SampathFileCalculatorDUNOConsoleInput.java

---
+++ SampathFileCalculatorDUNOConsoleInput.java
@@ -0,0 +1,13 @@
+// Use console class to input 2 integer and multiply
+import java.io.Console;
+
+public class SampathFileCalculatorDUNOConsoleInput {
+
+	public static void main(String[] args) {
+		Console con = System.console();
+		String enterNum = console.readLine("Enter a number: ");
+		
+		
+	}
+
+}
