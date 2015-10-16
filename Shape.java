671095165a65a43dc12d1a244c433f1af285ea13
sj49ner <sj49ner@gmail.com>
2015-10-16 10:19:29 -0700

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,13 @@
+public abstract class Shape {
+	
+   int numberOfSides;
+  double perimeter;
+   double area;
+   
+   public abstract double calculateArea();
+	
+   public abstract double calculatePerimeter();
+	
+  
+  
+}
